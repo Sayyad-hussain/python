fcadd0911561f9fc081aeb078a3f1ac62cffbc52
sayyad <sayyadhussain315@gmail.com>
2023-02-24 23:54:24 +0500

    program taht converts temperature form celsius to fahrenheit

---
+++ program3,28.py
@@ -0,0 +1,8 @@
+#write a python program that inputs temperature in celsius and converts it into farenheit.
+#using the formuls F=9/5 * C+32
+
+celsius = int(input("please enter temperature in celsius: "))
+
+fahrenheit = 9/5 *celsius+ 32
+
+print("Temperature in fahrenheit: ", fahrenheit)
