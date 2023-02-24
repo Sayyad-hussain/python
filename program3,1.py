799c09c1146103185c0bb501063c286f875438c4
sayyadhussain72 <77436639+Sayyad-hussain@users.noreply.github.com>
2023-02-24 22:37:19 +0500

    update
    write a python program that input the radius of a circle and display the circumference by using formula 2*pi*R .Store the value of pi=3.141 in a constant

--- program3,1.py
+++ program3,1.py
@@ -1,6 +1,4 @@
-# write a python program that input the radius of a circle and display the
-# circumference by using formula 2*pi*R
-# Store the value of pi=3.141 in a constant.
+# write a python program that input the radius of a circle and display the circumference by using formula 2*pi*R .Store the value of pi=3.141 in a constant.
 
 pi = 3.141
 r = float(input(print("please enter Radius of a circle: ")))
