3c882353926e85e22f6626618772b7439628f2fc
sayyad <sayyadhussain315@gmail.com>
2023-02-24 22:57:10 +0500

    write a python program that inputs dividend and divisor. it then calculate #and displays the quotient and remainder.

---
+++ program3,24.py
@@ -0,0 +1,13 @@
+#write a python program that inputs dividend and divisor. it then calculate
+#and displays the quotient and remainder.
+
+x = int(input("please enter divident: "))
+y = int(input("please enter divisor: "))
+
+q = x / y
+
+r = x % y
+
+print("Quotient: ", q)
+print("Remainder: ", r)
+
