5783943c2c685f0a0c70aeb842f8e5431ce150d4
sayyad <sayyadhussain315@gmail.com>
2023-02-25 11:21:13 +0500

    3,28 update

--- program3,28.py
+++ program3,28.py
@@ -5,4 +5,4 @@ celsius = int(input("please enter temperature in celsius: "))
 
 fahrenheit = 9/5 *celsius+ 32
 
-print("Temperature in fahrenheit: ", fahrenheit)
+print("Temperature in fahrenheit: ", fahrenheit,"f")
