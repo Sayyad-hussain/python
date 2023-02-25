03be14801ea37e15f892e3b104b2b278275b29c1
sayyad <sayyadhussain315@gmail.com>
2023-02-25 11:13:36 +0500

    write a python program that converts a persons height from inches to #centimeters using the formula. (2.54*height)

---
+++ program3,30.py
@@ -0,0 +1,8 @@
+#write a python program that converts a persons height from inches to 
+#centimeters using the formula. (2.54*height)
+
+height = int(input("please enter height in inches: "))
+
+h_centi = 2.54*height
+
+print("your height in centimeters : ",h_centi,"cm")
