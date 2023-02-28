6974b8aab0f786ce43b5167c69328b5b3d3ecb92
sayyad <sayyadhussain315@gmail.com>
2023-02-28 11:41:44 +0500

    program that check number is even or odd

---
+++ program4,5.py
@@ -0,0 +1,9 @@
+#write a python program that input a number and prints wheather it 
+#is even or odd
+
+no = int(input("please enter a number: "))
+
+if no%2==0:
+    print("Number is even")
+else:
+    print("Number is odd")
