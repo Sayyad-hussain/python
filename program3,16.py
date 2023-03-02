4b0bea4de715cffce2e226d2dd6539885be581cd
sayyad <sayyadhussain315@gmail.com>
2023-03-02 11:33:29 +0500

    #write a program that inputs a character and display wheather it is #vowel or not.

--- program3,16.py
+++
@@ -1,7 +0,0 @@
-#write a python program that reads length and breadth of a rectangle 
-#and print its area 
-
-length = float(input("please enter length of a rectangle: "))
-breadth = float(input("please enter breadth of a rectangle: "))
-area =  length * breadth
-print("Area of a rectangle: ", area,"area")
---
+++ program4,8.py
@@ -0,0 +1,9 @@
+
+#write a program that inputs a character and display wheather it is 
+#vowel or not.
+
+ch = str(input("please enter a character: "))
+
+if ch == 'A' and ch == 'a' and ch == 'E' and ch == 'e' and ch == 'I' and ch == 'i' and ch == 'O' and ch == 'o' and ch == 'U' and ch == 'u' :
+   print("Character is vowel...")
+ 
