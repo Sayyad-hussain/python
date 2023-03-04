a4ac9f7b5a47d4c5946fe5c7fa3fb7a0d7e273f3
sayyad <sayyadhussain315@gmail.com>
2023-03-04 22:27:04 +0500

    if-elif and else chapter start from 4,1

--- program4,1.py
+++ program4,1.py
@@ -1,13 +1,12 @@
 #write a program that reads two numbers and prints the sum and product of the numbers 
 #.if the first number is positive
 
-x = int(input("please enter first number: "))
-y = int(input("please enter second number: "))
-
-if x>0:
- sum = x+y
+a= int(input("please enter first number: "))
+b = int(input("please enter second number: "))
+if a>0:
+ sum = a+b
  print("Sum: ",sum)
- product = x*y
+ product = a*b
  print("product: ", product)
 else:
  print("first number is not positive")
