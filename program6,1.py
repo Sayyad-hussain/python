85636eca0227e7f4878eb9460e4d5f36002d7f35
sayyad <sayyadhussain315@gmail.com>
2023-03-07 11:55:15 +0500

    for loop chapter

---
+++ program6,1.py
@@ -0,0 +1,9 @@
+#write a python program that prints all positive odd numbers that less then twenty 
+#on a single line.
+
+i = 1
+
+for i in range(20):
+    if i % 2 !=0:
+        print(i, end=" ")
+    
---
+++ program6,2.py
@@ -0,0 +1,11 @@
+#write a program that display alphabets form A to Z using for loop.
+i = chr
+
+print("The Alphabets from A to Z are: ");
+
+for i in range(ord('A'), ord('Z') + 1 ):
+    print(chr(i), end=" ");
+print("\nThe Alphabets from a to z are: ");
+for i in range(ord('a'), ord('z') + 1 ):
+    print(chr(i), end=" ");
+		
